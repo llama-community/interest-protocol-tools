3602467c90d7a43b5dbbdc91cee69e5d47678c54
efecarranza <fcarr004@fiu.edu>
2023-03-01 10:59:32 -0500

    feat: scaffold initial hepler libraries

---
+++ src/address-book/IPAddressBook.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+import {IPEthereum} from "./IPEthereum.sol";
+
+import {IPGovernance} from "./IPGovernance.sol";
---
+++ src/listings/GenericListing.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IGenericListing} from './IGenericListing.sol';
+
+contract GenericListing is IGenericListing {
+
+}
