8f552132bcc656c6fd3b00e1be80c68e42366e64
efecarranza <fcarr004@fiu.edu>
2023-03-07 09:56:37 -0500

    add addresses to project

--- src/address-book/IPEthereum.sol
+++ src/address-book/IPEthereum.sol
@@ -2,5 +2,6 @@
 pragma solidity >=0.8.0;
 
 library IPEthereum {
-
+    address public constant VAULT_CONTROLLER = 0x4aaE9823Fb4C70490F1d802fC697F3ffF8D5CbE3;
+    address public constant VOTING_VAULT_CONTROLLER = 0xaE49ddCA05Fe891c6a5492ED52d739eC1328CBE2;
 }
--- src/address-book/IPGovernance.sol
+++ src/address-book/IPGovernance.sol
@@ -2,5 +2,5 @@
 pragma solidity >=0.8.0;
 
 library IPGovernance {
-
+    address public constant GOV = 0x266d1020A84B9E8B0ed320831838152075F8C4cA;
 }
